8493b7c50a8eac483060aaae1f8bcc84856e2861
1iyc <liycliycliyc@gmail.com>
2018-12-06 17:01:39 +0900

    Data Preprocesser

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # data_analyzer
 Data Analyze before Train
+
+# Rule of data_preprocess.py
+
+1. Replace "." to None
+2. Replace Specific Character(without "-") and Number to " "(White Space)
+3. Delete Duplicated Space
+4. Delete NA Word and Single Character
+5. Replace "-" to " "
---
+++ data_preprocess.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description="Preprocess Data Name")
+
+# I&O file
+parser.add_argument('--input_file', dest="input_file", type=str, default="./data/data.txt", help="Data File")
+parser.add_argument('--output_file', dest="output_file", type=str, default="./data/data_cleared.txt",
+                    help="Preprocessed Data Name File")
+
+parser.add_argument('--na_file', dest="na_file", type=str, default=None, help="Preprocess File with NA Word")
+
+args = parser.parse_args()
+
+def preprocess_data(input_file, output_file, na_file):
+    if na_file:
+        g = open(output_file + "_na.txt", 'w', encoding="utf-8")
+        with open(na_file, 'r', encoding="utf-8") as e:
+            na_list = [line.rstrip() for line in e]
+    else:
+        g = open(output_file, 'w', encoding="utf-8")
+
+    with open(input_file, 'r', encoding="utf-8") as f:
+        for line in f.readlines():
+            data = re.sub(r'[\.\-]', '', line.strip().upper())
+            data = re.sub(r'[^A-Z]', ' ', data)
+            #data = re.sub(r'[\-]', '', data)
+            #data = ' '.join(x.strip('-') for x in data.split())
+            if na_file:
+                data = ' '.join(x for x in data.split() if len(x) > 1 and x not in na_list)
+                g.write(data + '\n')
+            else:
+                data = ' '.join(x for x in data.split() if len(x) > 1)
+                g.write(data + '\n')
+
+
+def main():
+    preprocess_data(args.input_file, args.output_file, args.na_file)
+
+if __name__ == '__main__':
+    main()
+
