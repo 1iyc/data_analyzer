01abbc541b1f8a7d739b5b90ded6de8ce3618bdf
1iyc <liycliycliyc@gmail.com>
2018-12-12 17:58:16 +0900

    Dev Delete Wrong Data Name or Class Name in Data #data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -8,8 +8,7 @@ parser = argparse.ArgumentParser(description="Pre-processing Data Name")
 
 # I&O file
 parser.add_argument('--input_file', dest="input_file", type=str, default="./data/data.txt", help="Data File")
-# parser.add_argument('--output_file', dest="output_file", type=str, default="./data/data_cleared.txt",
-#                     help="Preprocessed Data Name File")
+parser.add_argument('--class_file', dest="class_file", type=str, default="./data/class.txt", help="Class File")
 
 # Pre-process Mode
 parser.add_argument('--DN', dest="DN", type=bool, default=False,
@@ -28,68 +27,81 @@ parser.add_argument('--NA', dest="NA", type=str, default=None,
 args = parser.parse_args()
 
 
-def pre_process_data(input_file, dn, ds, sc, dc, na_file):
-#    if na_file:
-#        with open(na_file, 'r', encoding="utf-8") as e:
-#            na_list = [line.rstrip() for line in e]
-#    else:
-
+def pre_process_data(input_file, class_file, dn, ds, sc, dc, na_file):
     dir_path = os.path.dirname(os.path.realpath(input_file))
     folder_name = "Pre_Processed" + "_DN"*dn + "_DS"*ds + "_SC"*sc + "_DC"*dc + "_NA"*bool(na_file)
     output_path = os.path.join(dir_path, folder_name)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
+    data_list = list(open(input_file, 'r', encoding="utf-8").readlines())
+    class_list = list(open(class_file, 'r', encoding="utf-8").readlines())
+
     g = open(os.path.join(output_path, "data.txt"), 'w', encoding="utf-8")
+    h = open(os.path.join(output_path, "class.txt"), 'w', encoding="utf-8")
 
     if na_file:
         with open(na_file, 'r', encoding="utf-8") as e:
             na_list = [line.rstrip() for line in e]
 
-    with open(input_file, 'r', encoding="utf-8") as f:
-        for line in f.readlines():
-            # Phase 1&2.
-            if dn and not ds:
-                folder_name += "_DN"
-                data = re.sub(r'[\.\-]', '', line.strip().upper())
-            else:
-                data = re.sub(r'[\.]', '', line.strip().upper())
+    for i, line in enumerate(data_list):
+        try:
+            class_data = int(class_list[i].strip())
+            if class_data < 10000 or class_data > 999999:
+                del class_list[i]
+                del data_list[i]
+                continue
+        except ValueError:
+            del class_list[i]
+            del data_list[i]
+            continue
 
-            # Phase 3.
-            if ds and not dn:
-                folder_name += "_DS"
-                data = re.sub(r'[^A-Z]', ' ', data)
-            else:
-                data = re.sub(r'[^A-Z\-]', ' ', data)
+        # Phase 1&2.
+        if dn and not ds:
+            data = re.sub(r'[\.\-]', '', line.strip().upper())
+        else:
+            data = re.sub(r'[\.]', '', line.strip().upper())
 
-            if not ds and not dn:
-                data = ' '.join(x.strip('-') for x in data.split())
+        # Phase 3.
+        if ds and not dn:
+            data = re.sub(r'[^A-Z]', ' ', data)
+        else:
+            data = re.sub(r'[^A-Z\-]', ' ', data)
 
-            if sc and dc and na_file:
-                data = ' '.join(x for x in data.split() if len(x) > 1 and len(set(x)) > 1 and x not in na_list)
-            elif sc and dc:
-                data = ' '.join(x for x in data.split() if len(x) > 1 and len(set(x)) > 1)
-            elif sc and na_file:
-                data = ' '.join(x for x in data.split() if len(x) > 1 and x not in na_list)
-            elif dc and na_file:
-                data = ' '.join(x for x in data.split() if len(set(x)) > 1 and x not in na_list)
-            elif sc:
-                data = ' '.join(x for x in data.split() if len(x) > 1)
-            elif dc:
-                data = ' '.join(x for x in data.split() if len(set(x)) > 1)
-            elif na_file:
-                data = ' '.join(x for x in data.split() if x not in na_list)
-            else:
-                data = ' '.join(x for x in data.split())
+        if not ds and not dn:
+            data = ' '.join(x.strip('-') for x in data.split())
 
-            g.write(data + '\n')
+        if sc and dc and na_file:
+            data = ' '.join(x for x in data.split() if len(x) > 1 and len(set(x)) > 1 and x not in na_list)
+        elif sc and dc:
+            data = ' '.join(x for x in data.split() if len(x) > 1 and len(set(x)) > 1)
+        elif sc and na_file:
+            data = ' '.join(x for x in data.split() if len(x) > 1 and x not in na_list)
+        elif dc and na_file:
+            data = ' '.join(x for x in data.split() if len(set(x)) > 1 and x not in na_list)
+        elif sc:
+            data = ' '.join(x for x in data.split() if len(x) > 1)
+        elif dc:
+            data = ' '.join(x for x in data.split() if len(set(x)) > 1)
+        elif na_file:
+            data = ' '.join(x for x in data.split() if x not in na_list)
+        else:
+            data = ' '.join(x for x in data.split())
+
+        if data == '':
+            del class_list[i]
+            del data_list[i]
+            continue
+
+        g.write(data + '\n')
+        h.write(class_list[i].strip() + '\n')
 
 
 def main():
     if args.DN and args.DS:
         print("Select Either DN or DS")
         exit()
-    pre_process_data(args.input_file, args.DN, args.DS, args.SC, args.DC, args.NA)
+    pre_process_data(args.input_file, args.class_file, args.DN, args.DS, args.SC, args.DC, args.NA)
 
 
 if __name__ == '__main__':
---
+++ delete_white_data.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Data Name Clearing")
+
+# I&O file
+parser.add_argument('--data_file', dest="data_file", type=str, default="./data/data.txt", help="Data File")
+parser.add_argument('--class_file', dest="class_file", type=str, default="./data/class.txt",
+                    help="Class File")
+parser.add_argument('--output_file', dest="output_file", type=str, default="./data/data_static.txt", help="Output File")
+
+args = parser.parse_args()
+
+def data_verify(data_file, class_file, output_file):
+    data_list = list(open(data_file, 'r', encoding="utf-8").readlines())
+    class_list = list(open(class_file, 'r', encoding="utf-8").readlines())
+
+    data = dict()
+    total_data_count = len(data_list)
+    count = 0
+
+    print("Dictionary Making...")
+    for i in range(len(data_list)):
+        data_line = data_list[i].strip()
+        class_line = class_list[i].strip()
+        if data_line in data:
+            if class_line in data[data_line]:
+                data[data_line][class_line] += 1
+            else:
+                data[data_line][class_line] = 1
+        else:
+            data[data_line] = dict()
+            data[data_line][class_line] = 1
+        count += 1
+        if count % int(total_data_count * 0.01) == 0:
+            print(str(int(count/total_data_count*100)) + "%....")
+
+    print("Static File Writing...")
+    with open(output_file, 'w', encoding="utf-8") as f:
+        for i in data:
+            record = i + "\t"
+            # f.write(i + "\t")
+            total = 0
+            for k in data[i]:
+                total += data[i][k]
+            for k in sorted(data[i], key=data[i].get, reverse=True):
+                # f.write(k + "\t" + str(data[i][k]) + "\t" + str(round(data[i][k]/total*100, 2)) + "%\t")
+                record += k + "\t" + str(data[i][k]) + "\t" + str(round(data[i][k]/total*100, 2)) + "%\t"
+            f.write(str(total) + "\t" + record + "\n")
+
+def main():
+    data_verify(args.data_file, args.class_file, args.output_file)
+
+if __name__ == '__main__':
+    main()
