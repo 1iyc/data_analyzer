cca6d06043bde7fa9b3bbcafb2c97f4367b243a2
1iyc <liycliycliyc@gmail.com>
2019-01-18 14:55:02 +0900

    Dev Shuffle Data

--- extract_shuffle.py
+++ extract_shuffle.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
-import re
+import random
 
 parser = argparse.ArgumentParser(description="Data Name Clearing")
 
@@ -10,32 +10,19 @@ parser.add_argument('--input_file', dest="input_file", type=str, default="./data
 parser.add_argument('--output_file', dest="output_file", type=str, default="./data/data_cleared.txt",
                     help="Cleared Data Name File")
 
-parser.add_argument('--na_file', dest="na_file", type=str, default=None, help="Cleared File with NA Word")
-
 args = parser.parse_args()
 
-def clear_data(input_file, output_file, na_file):
+def data_shuffle(input_file, output_file):
     g = open(output_file, 'w', encoding="utf-8")
-    if na_file:
-        h = open(output_file + "_na_cleared.txt", 'w', encoding="utf-8")
-        with open(na_file, 'r', encoding="utf-8") as e:
-            na_list = [line.rstrip() for line in e]
+    data_list = list(open(input_file, "r", encoding='utf-8').readlines())
 
-    with open(input_file, 'r', encoding="utf-8") as f:
-        for line in f.readlines():
-            data = re.sub(r'[^ABCDEFGHIJKLMNOPQRSTUVWXYZ-]', ' ', line.strip().upper())
-            data = ' '.join(x.strip('-') for x in data.split())
-            data = ' '.join(x for x in data.split() if len(x) > 1 and len(set(x)) > 1)
+    data_list = random.sample(data_list, len(data_list))
 
-            g.write(data + '\n')
-
-            if na_file:
-                data = ' '.join(x for x in data.split() if x not in na_list)
-                h.write(data + '\n')
-
+    for i in range(400000):
+        g.write(data_list[i])
 
 def main():
-    clear_data(args.input_file, args.output_file, args.na_file)
+    data_shuffle(args.input_file, args.output_file)
 
 if __name__ == '__main__':
     main()
